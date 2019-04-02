27d705a62a68babd5b522a1684fa2a502d266553
ineesgoncalvees <ines98goncalves@gmail.com>
2019-04-02 09:35:47 +0100

    Segundo commit

---
+++ Starligth/Assets/Scripts/GravityBody.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+    private GravityAtractor moon;
+
+    private void Awake()
+    {
+        moon = GameObject.FindGameObjectWithTag("Moon").GetComponent<GravityAtractor>();
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        moon.Attract(transform);
+    }
+}
