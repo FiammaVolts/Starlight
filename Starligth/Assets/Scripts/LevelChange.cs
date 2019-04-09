468e89c01477a8aec116627ca47cfcb35aeaddf0
ineesgoncalvees <ines98goncalves@gmail.com>
2019-04-09 08:43:33 +0100

    ja muda de nivel

---
+++ Starligth/Assets/Scripts/LevelChange.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChange : MonoBehaviour
+{
+    public GameObject text;
+    public GameObject fragment;
+    
+    void Start()
+    {
+        text.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            text.SetActive(true);
+            if(text.activeInHierarchy == true && Input.GetButtonDown("Interact"))
+            {
+                fragment.SetActive(false);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+
+    private void OnTriggerExit()
+    {
+        text.SetActive(false);
+    }
+}
---
+++ Starligth/Assets/Scripts/PatrolAI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolAI : MonoBehaviour
+{
+    public float speed;
+
+    private bool movingRight = true;
+
+    public Transform groundDetection;
+
+    void Update()
+    {
+        MoveAI();        
+    }
+
+    private void MoveAI()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector3.down, 2f);
+
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+}
